927024c0b1bdb69db422e262eff800b809508749
Omar <ghoneim.omar88@gmail.com>
2022-07-02 10:00:01 -0600

    Added 8th and 9th video code

---
+++ 8 - Add New Lesson 2/pythonic/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_ckeditor import CKEditor
+from flask_modals import Modal
+app = Flask(__name__)
+
+app.config[
+    "SECRET_KEY"
+] = "62913a7dac3933f87a84626fcdeaaf9e2653f0a000843efd9bf2b31ba4767402"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pythonic.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+migrate = Migrate(app, db)
+login_manager = LoginManager(app)
+ckeditor = CKEditor(app)
+modal = Modal(app)
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
+from pythonic import routes
---
+++ 8 - Add New Lesson 2/pythonic/routes.py
@@ -0,0 +1,262 @@
+import secrets
+from PIL import Image
+import os
+from pythonic.models import User, Lesson, Course
+from flask import render_template, url_for, flash, redirect, request, session
+from pythonic.forms import (
+    NewCourseForm,
+    NewLessonForm,
+    RegistrationForm,
+    LoginForm,
+    UpdateProfileForm,
+)
+from pythonic import app, bcrypt, db
+from flask_modals import render_template_modal
+from flask_login import (
+    login_required,
+    login_user,
+    current_user,
+    logout_user,
+    login_required,
+)
+
+
+lessons = [
+    {
+        "title": "Request Library Course",
+        "course": "Python",
+        "author": "Omar",
+        "thumbnail": "thumbnail.jpg",
+    },
+    {
+        "title": "Request Library Course",
+        "course": "Python",
+        "author": "Omar",
+        "thumbnail": "thumbnail.jpg",
+    },
+    {
+        "title": "Request Library Course",
+        "course": "Python",
+        "author": "Omar",
+        "thumbnail": "thumbnail.jpg",
+    },
+    {
+        "title": "Request Library Course",
+        "course": "Python",
+        "author": "Omar",
+        "thumbnail": "thumbnail.jpg",
+    },
+    {
+        "title": "Request Library Course",
+        "course": "Python",
+        "author": "Omar",
+        "thumbnail": "thumbnail.jpg",
+    },
+    {
+        "title": "Request Library Course",
+        "course": "Python",
+        "author": "Omar",
+        "thumbnail": "thumbnail.jpg",
+    },
+]
+
+courses = [
+    {
+        "name": "Python",
+        "icon": "python.svg",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!",
+    },
+    {
+        "name": "Data Analysis",
+        "icon": "analysis.png",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!",
+    },
+    {
+        "name": "Machine Learning",
+        "icon": "machine-learning.png",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!",
+    },
+    {
+        "name": "Web Design",
+        "icon": "web.png",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!",
+    },
+    {
+        "name": "Blockchain",
+        "icon": "blockchain.png",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!",
+    },
+    {
+        "name": "Tips & Tricks",
+        "icon": "idea.png",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quidem nihil dolor officiis at magni!",
+    },
+]
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_name = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, "static/user_pics", picture_name)
+    output_size = (150, 150)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_name
+
+
+def get_previous_next_lesson(lesson):
+    course = lesson.course_name
+    for lsn in course.lessons:
+        if lsn.title == lesson.title:
+            index = course.lessons.index(lsn)
+            previous_lesson = course.lessons[index - 1] if index > 0 else None
+            next_lesson = (
+                course.lessons[index + 1] if index < len(course.lessons) - 1 else None
+            )
+            break
+    return previous_lesson, next_lesson
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    lessons = Lesson.query.all()
+    courses = Course.query.all()
+    return render_template("home.html", lessons=lessons, courses=courses)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode(
+            "utf-8"
+        )
+        user = User(
+            fname=form.fname.data,
+            lname=form.lname.data,
+            username=form.username.data,
+            email=form.email.data,
+            password=hashed_password,
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash(f"Account created successfully for {form.username.data}", "success")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get("next")
+            flash("You have been logged in!", "success")
+            return redirect(next_page) if next_page else redirect(url_for("home"))
+        else:
+            flash("Login Unsuccessful. Please check credentials", "danger")
+    return render_template("login.html", title="Login", form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+
+@app.route("/dashboard", methods=["GET"])
+@login_required
+def dashboard():
+    return render_template("dashboard.html", title="Dashboard", active_tab=None)
+
+
+@app.route("/dashboard/profile", methods=["GET", "POST"])
+@login_required
+def profile():
+    profile_form = UpdateProfileForm()
+    if profile_form.validate_on_submit():
+        if profile_form.picture.data:
+            picture_file = save_picture(profile_form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = profile_form.username.data
+        current_user.email = profile_form.email.data
+        current_user.bio = profile_form.bio.data
+        db.session.commit()
+        flash("Your profile has been updated", "success")
+        return redirect(url_for("profile"))
+    elif request.method == "GET":
+        profile_form.username.data = current_user.username
+        profile_form.email.data = current_user.email
+        profile_form.bio.data = current_user.bio
+    image_file = url_for("static", filename=f"user_pics/{current_user.image_file}")
+    return render_template(
+        "profile.html",
+        title="Profile",
+        profile_form=profile_form,
+        image_file=image_file,
+        active_tab="profile",
+    )
+
+
+@app.route("/dashboard/new_lesson", methods=["GET", "POST"])
+@login_required
+def new_lesson():
+    new_lesson_form = NewLessonForm()
+    new_course_form = NewCourseForm()
+    form = ""
+    flag = session.pop("flag", False)
+    if "content" in request.form:
+        form = "new_lesson_form"
+    elif "description" in request.form:
+        form = "new_course_form"
+
+    if form == "new_lesson_form" and new_lesson_form.validate_on_submit():
+        lesson_slug = str(new_lesson_form.slug.data).replace(" ", "-")
+        course = new_lesson_form.course.data
+        lesson = Lesson(
+            title=new_lesson_form.title.data,
+            content=new_lesson_form.content.data,
+            slug=lesson_slug,
+            author=current_user,
+            course_name=course,
+        )
+        db.session.add(lesson)
+        db.session.commit()
+        flash("Your lesson has been created!", "success")
+        return redirect(url_for("new_lesson"))
+
+    elif form == "new_course_form" and new_course_form.validate_on_submit():
+        course_title = str(new_course_form.title.data).replace(" ", "-")
+        course = Course(
+            title=new_course_form.title.data,
+            description=new_course_form.description.data,
+        )
+        db.session.add(course)
+        db.session.commit()
+        session["flag"] = True
+        flash("New Course has been created!", "success")
+        return redirect(url_for("dashboard"))
+
+    modal = None if flag else "newCourse"
+    return render_template_modal(
+        "new_lesson.html",
+        title="New Lesson",
+        new_lesson_form=new_lesson_form,
+        new_course_form=new_course_form,
+        active_tab="new_lesson",
+        modal=modal,
+    )
